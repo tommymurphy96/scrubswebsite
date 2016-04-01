f88ca8ede8561a6bcf14781da769ccee09930887
JasonDupuis95 <dupuis1439@saskpolytech.ca>
2016-04-01 11:56:17 -0600

    Header, Footer, Nav Bar, Copyright changes.

--- Buncha_Scrubs/Buncha_Scrubs/Master.Master.designer.cs
+++ Buncha_Scrubs/Buncha_Scrubs/Master.Master.designer.cs
@@ -13,15 +13,6 @@ namespace Buncha_Scrubs {
     public partial class Master {
         
         /// <summary>
-        /// head control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
-        
-        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
@@ -29,14 +20,5 @@ namespace Buncha_Scrubs {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// ContentPlaceHolder1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
