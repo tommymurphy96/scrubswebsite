1d70829a6ea2895095e71949fd60fac14e4dc933
darshitagangwani <darshi_gangwani@hotmail.com>
2016-04-13 10:52:28 -0600

    changes in master

--- Buncha_Scrubs/Buncha_Scrubs/Master.Master.designer.cs
+++ Buncha_Scrubs/Buncha_Scrubs/Master.Master.designer.cs
@@ -7,10 +7,19 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Buncha_Scrubs {
+namespace BunchaScrubs {
     
     
     public partial class Master {
+        
+        /// <summary>
+        /// Head1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlHead Head1;
         
         /// <summary>
         /// head control.
@@ -29,14 +38,5 @@ namespace Buncha_Scrubs {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// ContentPlaceHolder1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
