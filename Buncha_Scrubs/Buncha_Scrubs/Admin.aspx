6eea8da563b625273426c297a77b9b776650cd17
JasonDupuis95 <dupuis1439@saskpolytech.ca>
2016-05-04 11:25:20 -0600

    Admin

--- Buncha_Scrubs/Buncha_Scrubs/Admin.aspx
+++ Buncha_Scrubs/Buncha_Scrubs/Admin.aspx
@@ -3,13 +3,25 @@
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
+<head id="Head1" runat="server">
     <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
-    
+        <h1>My Image Galery</h1>
+        <asp:DataList ID="dlstImages" runat="server" RepeatColumns="3" RepeatDirection="Horizontal">
+            <ItemTemplate>
+                <asp:Image ID="Image1" runat="server" style="width:200px" ImageUrl='<%# Eval("Name", "~/Prints/{0}") %>' />
+                <br />
+                <%# Eval("Name") %><br />
+                <a href='FileUploads.aspx?fileToDelete=<%# Eval("Name")%>'>Delete</a> 
+            </ItemTemplate>
+        </asp:DataList>
+
+        <hr/>
+        <asp:FileUpload ID="fupUpload" runat="server" /> &nbsp<asp:Button ID="btnSave" runat="server" Text="Upload" OnClick="btnSave_Click" />
+
     </div>
     </form>
 </body>
--- Buncha_Scrubs/Buncha_Scrubs/Admin.aspx.cs
+++ Buncha_Scrubs/Buncha_Scrubs/Admin.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,10 +8,50 @@ using System.Web.UI.WebControls;
 
 namespace Buncha_Scrubs
 {
-    public partial class Admin : System.Web.UI.Page
+    public partial class FileUploads : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        protected void Page_PreRender(object sender, EventArgs e)
         {
+
+            DirectoryInfo dir = new DirectoryInfo(MapPath("~/Prints/"));
+            dlstImages.DataSource = dir.GetFiles();
+            dlstImages.DataBind();
+
+            String fileName = Request.QueryString["fileToDelete"];
+            if (fileName != null)
+            {
+                File.Delete(dir + fileName);
+                Response.Redirect("FileUploads.aspx");
+            }
+        }
+
+        private bool CheckFileType(string fileName)
+        {
+            string ext = Path.GetExtension(fileName);
+            switch (ext.ToLower())
+            {
+                case ".gif":
+                    return true;
+                case ".png":
+                    return true;
+                case ".jpg":
+                    return true;
+                case ".jpeg":
+                    return true;
+                default:
+                    return false;
+            }
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            string filePath = "~/UploadedImages/" + fupUpload.FileName;
+
+            if (CheckFileType(filePath))
+            {
+                fupUpload.SaveAs(MapPath(filePath));
+            }
 
         }
     }
