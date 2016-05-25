6a84a85d3cd6c699195dba8fc648075376706dd4
darshitagangwani <darshi_gangwani@hotmail.com>
2016-05-25 10:26:59 -0600

    Revert "some dumb changes"
    This reverts commit 0f715bbc05c661b6b8f6f400bb0158c50dc8fbec.

--- Buncha_Scrubs/Buncha_Scrubs/Master.Master.designer.cs
+++ Buncha_Scrubs/Buncha_Scrubs/Master.Master.designer.cs
@@ -40,6 +40,15 @@ namespace Buncha_Scrubs {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder Header;
         
         /// <summary>
+        /// Carousel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Carousel;
+        
+        /// <summary>
         /// MiddleContents control.
         /// </summary>
         /// <remarks>
